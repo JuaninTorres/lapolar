cf0cc7d234955f5d972bf6b8840c5bdc0b495713
Juan Torres <juanin.torres@gmail.com>
2013-06-23 20:26:41 +0000

    Migraciones faltantes (otras)

--- app/database/migrations/2013_06_23_162448_create_dataposiciones_table.php
+++ app/database/migrations/2013_06_23_162448_create_dataposiciones_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePosicionesregistrosTable extends Migration {
+class CreateDataposicionesTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,7 +12,7 @@ class CreatePosicionesregistrosTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('posicionesregistros', function(Blueprint $table)
+		Schema::create('dataposiciones', function(Blueprint $table)
 		{
 			$table->increments('id');
 			$table->timestamps();
@@ -26,7 +26,7 @@ class CreatePosicionesregistrosTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('posicionesregistros');
+		Schema::drop('dataposiciones');
 	}
 
 }
